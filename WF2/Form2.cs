d91d358b7d4e4db59ce82a63d027518416098896
Ladislav Sopko <lsopko@3di.it>
2016-03-04 16:05:12 +0100

    just created

---
+++ WF2/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors.Repository;
+
+namespace WF2
+{
+	public partial class Form2 : Form
+	{
+
+		test1Entities1 ctx;
+		test1Entities ctx1;
+		
+		public Form2()
+		{
+			
+			InitializeComponent();
+
+			ctx = new test1Entities1();
+			ctx1 = new test1Entities();
+			
+			
+
+			BindingSource bs = new BindingSource();
+			bs.DataSource = ctx1.bab.Where(s => s.id == "00000100").ToList();// typeof(bab);
+			//bs.AddNew();
+
+			xwcs.core.DataLayout.DataLayoutExtender de = new xwcs.core.DataLayout.DataLayoutExtender(dataLayoutControl1);
+			de.GetFieldQueryable += (sender, d) =>
+			{
+				switch(d.FieldName) {
+					case "cc":
+						d.DataSource = ctx.names.ToList();
+						break;
+                }
+			};
+			
+            dataLayoutControl1.DataSource = bs;
+
+			
+		}		
+	}
+}
---
+++ WF2/attrs.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace WF2
+{
+	public class bab_meta
+	{
+		const string MainGroup = "{tabs}";
+		const string Resp = MainGroup + "/Responsabilita";
+		const string MG_Main = MainGroup + "/<Doc>/Main";
+		const string MG_Parole = MainGroup + "/<Doc>/Parole";
+		const string MG_Testo = MainGroup + "/<Doc>/Testo";
+
+		[Display(Name = "n° Doc", GroupName = MG_Main + "/<IdRandom->", Order = 1)]
+		[System.ComponentModel.ReadOnly(true)]
+		public int ndoc { get; set; }
+
+		[xwcs.core.DataLayout.Attributes.Style(BackgrounColor = 0xFFADFF2F)]
+		[Display(Name = "Id", GroupName = MG_Main + "/<IdRandom->")]
+		[System.ComponentModel.ReadOnly(true)]
+		public string id { get; set; }
+
+		[xwcs.core.DataLayout.Attributes.Style(BackgrounColor = 0xFFADFF2F)]
+		[Display(Name = "Random", GroupName = MG_Main + "/<IdRandom->")]
+		[Range(0, 1000, ErrorMessage = "Fuori range!")]
+		public int random { get; set; }
+		[Display(Name = "Parole", GroupName = MG_Parole)]
+		public string dictionary { get; set; }
+		[Display(Name = "Testo", GroupName = MG_Parole), DataType(DataType.MultilineText)]
+		public string text { get; set; }
+		[Display(Name = "RPA", GroupName = Resp)]
+		public string rpa { get; set; }
+		[Display(Name = "CC", GroupName = Resp)]
+		[xwcs.core.DataLayout.Attributes.DbLookup(DisplayMember = "Name", ValueMember = "Name")]
+		public string cc { get; set; }
+		[Display(Name = "DXP", GroupName = Resp)]
+		public string dxp { get; set; }
+		[System.ComponentModel.ReadOnly(true)]
+		[Display(Name = "xml", GroupName = MainGroup + "/XML"), DataType(DataType.MultilineText)]
+		public string xml { get; set; }
+		[Display(AutoGenerateField = false)]
+		public string extra { get; set; }
+	}
+
+	public class bab_ext {
+		public const UInt32 greenColor = 0xff008000;
+
+		[xwcs.core.DataLayout.Attributes.Style(BackgrounColor = greenColor)]
+		[Display(Name = "N_RPA", GroupName = "n")]
+		public string n_rpa { get; set; }
+		[Display(Name = "N_CC", GroupName = "n")]
+		public string n_cc { get; set; }
+		[xwcs.core.DataLayout.Attributes.Style(BackgrounColor = greenColor)]
+		[Display(Name = "N_DXP", GroupName = "n")]
+		public string n_dxp { get; set; }
+		[xwcs.core.DataLayout.Attributes.Style(BackgrounColor = 0xFFADFF2F)]
+		[Display(Name = "N_Date", GroupName = "n")]
+		public DateTime n_data { get; set; }
+	}
+
+	[MetadataType(typeof(bab_meta))]
+	public partial class bab {
+
+		[Display(Name = "Embeded", GroupName = "{tabs}/Embeded", Order = 4)]
+		public bab_ext Bab_Ext{get ; set; }
+	}
+
+
+
+
+	public partial class bab_view_data
+	{
+		[Key]
+		[Display(Name = "n° Doc")]
+		public int ndoc { get; set; }
+		[Display(Name = "Id")]
+		public string id { get; set; }
+		[Display(Name = "Random")]
+		public int random { get; set; }
+		[Display(Name = "Parole", GroupName = "{tabs}")]
+		public string dictionary { get; set; }
+
+
+	}
+}
